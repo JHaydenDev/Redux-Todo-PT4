448f132a460c2594aa9fc98400dcead2ceae5447
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-04-16 22:04:00 -0500

    Created Reducers

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,19 +1,15 @@
-    
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import App from "./App";
-import { Reducer } from "./Reducers/TodoReducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 
-const store = createStore(
-    Reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-ReactDOM.render(
-  <Provider store={store}>
+ ReactDOM.render(
+  <Provider store={createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
     <App />
-  </Provider>,
-  document.getElementById("root")
-);
+  </Provider>, document.getElementById('root'));
+
+ registerServiceWorker();
