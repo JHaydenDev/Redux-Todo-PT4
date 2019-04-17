aa2127d50b9e87a5a07e8e7fce1ff3f9c11298eb
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-04-16 21:52:33 -0500

    Created provider, working on creating components.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,28 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { addTodo } from './Actions/TodoActions';
+import TodoList from './Components/TodoList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: ''
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.updateNewTodo = this.updateNewTodo.bind(this);
+  }
+
+  addTodo(event) {
+    event.preventDefault();
+    this.props.addTodo({
+      value: this.state.newTodo,
+      complete: false
+    });
+    this.setState({
+      newTodo: ''
+    });
+  }
+
+  updateNewTodo(event) {
+    this.setState({
+      newTodo: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Joe's stuff he has been procrastinating.</h1>
+        <form onSubmit={this.addTodo}>
+          <input
+          placeholder="Lets do some stuff!!!"
+          onChange={this.updateNewTodo}  
+          value={this.state.newTodo}
+          />
+        </form>
+        <button onClick={this.addTodo}>Add Todo</button>
+        <TodoList todos={this.props.todos} />
       </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    todos: state.todos
+  };
+};
+export default connect(mapStateToProps, { addTodo })(App);
